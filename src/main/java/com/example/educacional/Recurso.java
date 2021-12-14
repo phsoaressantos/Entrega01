aa52bf51fbd7fe88b496c5ac6dbf425a1662d760
Paulo Henrique Santos <paulosantos@id.uff.br>
2021-12-14 07:43:47 -0300

    Initial commit

---
+++ src/main/java/com/example/educacional/Recurso.java
@@ -0,0 +1,74 @@
+package com.example.educacional;
+
+import javax.persistence.*;
+
+@Entity
+
+public class Recurso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String dataCriacao;
+    private String dataRegistro;
+
+    public Recurso(String titulo, String descricao, String dataCriacao, String dataRegistro) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataCriacao = dataCriacao;
+        this.dataRegistro = dataRegistro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getDataRegistro() {
+        return dataRegistro;
+    }
+
+    public void setDataRegistro(String dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Recurso{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataCriacao='" + dataCriacao + '\'' +
+                ", dataRegistro='" + dataRegistro + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/example/educacional/EducacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducacionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
