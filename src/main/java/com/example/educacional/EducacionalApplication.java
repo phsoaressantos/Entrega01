2a30b2dafc0a0bd7729ee020d2546f0ca2c718be
Paulo Henrique Santos <paulosantos@id.uff.br>
2021-12-14 08:11:04 -0300

    2 commit

--- src/main/java/com/example/educacional/EducacionalApplication.java
+++ src/main/java/com/example/educacional/EducacionalApplication.java
@@ -1,7 +1,9 @@
 package com.example.educacional;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EducacionalApplication {
@@ -10,4 +12,15 @@ public class EducacionalApplication {
         SpringApplication.run(EducacionalApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run (RecursoRepository repository){
+        return (args -> {
+         insertJavaAdvocates(repository);
+         System.out.println(repository.findAll());
+        });
+    }
+
+    private void insertJavaAdvocates(RecursoRepository repository){
+        repository.save(new Recurso("Teste","Descricao do recurso", "02122021","data"));
+    }
 }
---
+++ src/main/java/com/example/educacional/RecursoRepository.java
@@ -0,0 +1,6 @@
+package com.example.educacional;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecursoRepository extends CrudRepository<Recurso, Long> {
+}
